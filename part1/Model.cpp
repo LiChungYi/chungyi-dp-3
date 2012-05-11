3dfbced62d2029da6cdfe84c808ad2aaecba0b74
Johnny31525 <Johnny31525@ce91877d-3c9d-76b0-113a-0c24f2962d98>
2012-05-11 04:19:50 +0000

    part1 finish

--- part1/Model.cpp
+++ part1/Model.cpp
@@ -4,38 +4,18 @@
 #include "xbug.hpp"
 #include <iostream>
 
-Model::Model(std::string modelName, Imp* imp)
+Model::Model(std::string modelName, Factory* factory)
 {
-	model_name = modelName ;
-	model_no = imp->OpenModel(model_name) ;
+	model_name = modelName;
+	model_no = factory->OpenModel(model_name);
 	if( model_no == 0){
 			throw xbug("Model: Model "+model_name+" not exists.");
 	}
-    std::vector< std::pair<ShapeType, int> > list = imp->getList(model_no);
+    std::vector< std::pair<ShapeType, int> > list = factory->getList(model_no);
 
 	for(std::vector< std::pair<ShapeType, int> >::iterator iter = list.begin(); iter != list.end(); iter++)
     {
-		Feature* f ;
-		switch(iter->first)
-		{
-			case SLOT:
-				f = new SlotFeature(model_no, iter->second, imp) ;
-				break;
-			case HOLE:
-				f = new HoleFeature(model_no, iter->second, imp) ;
-				break;
-			case CUTOUT:
-				f = new CutoutFeature(model_no, iter->second, imp) ;
-				break;
-			case IRREGULAR:
-				f = new IrregularFeature(model_no, iter->second, imp) ;
-				break;
-			case SPECIAL:
-				f = new SpecialFeature(model_no, iter->second, imp) ;
-				break;
-			default:
-				{}
-		}
+		Feature* f = factory->generate(iter, model_no);
 		features.push_back(f) ;
 	}
 }
